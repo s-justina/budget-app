eafc33c100971dfc6fdb792548c21bd360c2ec81
s-justina <jus.sobcz@gmail.com>
2020-07-29 18:10:46 +0200

    create Navigation translating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {ThemeProvider} from "styled-components";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 import GlobalStyles from './index.css';
 import {Navigation} from "components";
 import theme from 'utils/theme'
+import {Wrapper} from "components";
+
 
 function App() {
+    const { i18n } = useTranslation();
     return (
 
         <ThemeProvider theme={theme}>
@@ -15,18 +19,35 @@ function App() {
                 <Navigation items={[
                     {content: 'Homepage', to: '/'},
                     {content: 'Budget', to: '/budget'}
-                ]}/>
-                <Switch>
-                    <Route exact path='/'>
-                        Homepage
-                    </Route>
-                    <Route part='/budget'>
-                        Budget page
-                    </Route>
-                </Switch>
+                ]}
+                RightElement={(
+                    <div>
+                        <button onClick={()=>i18n.changeLanguage('pl')}>pl</button>
+                        <button onClick={()=>i18n.changeLanguage('en')}>en</button>
+                    </div>
+                )}
+                />
+                <Wrapper>
+                    <Switch>
+                        <Route exact path='/'>
+                            Homepage
+                        </Route>
+                        <Route part='/budget'>
+                            Budget page
+                        </Route>
+                    </Switch>
+                </Wrapper>
             </Router>
         </ThemeProvider>
     );
 }
 
-export default App;
+function RootApp() {
+return(
+    <React.Suspense fallback={'loading...'}>
+        <App/>
+    </React.Suspense>
+)
+}
+
+export default RootApp;
--- src/components/Navigation/Navigation.css.js
+++ src/components/Navigation/Navigation.css.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
+import Wrapper from "../Wrapper";
 
 export const Container = styled.div`
 display: flex;
 padding: ${({theme}) => theme.spacing.sm}px 0;
 justify-content: space-between;
 background-color: ${({theme}) => theme.colors.gray.light};
+`;
+
+export const NavigationWrapper = styled(Wrapper)`
+display: flex;
+justify-content: space-between;
 `;
 
 export const List = styled.ul`
